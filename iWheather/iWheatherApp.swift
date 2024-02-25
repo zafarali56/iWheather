32e3a37108cfb42d4ef7dcf765acb34aef6f42ea
zafar ali <zafaralyjamali722@gmail.com>
2024-02-25 21:09:20 +0500

    Initial Commit

---
+++ iWheather/iWheatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWheatherApp.swift
+//  iWheather
+//
+//  Created by Zafar Ali on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iWheatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
